0b655b311239deb7f905f0c6397c0fb801d7f39f
Darron Froese <darron@froese.org>
2013-04-14 14:05:03 -0600

    License.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             "jetty"
 maintainer       "Darron Froese"
 maintainer_email "darron@froese.org"
-license          "All rights reserved"
+license          "apachev2"
 description      "Installs/Configures jetty for use with Solr"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.3.0"
